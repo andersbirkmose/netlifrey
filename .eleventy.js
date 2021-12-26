5df40b3b367ca3f4692c9f372d0c786f8933edf7
Anders Frey Birkmose <anders@birkmose.com>
2021-12-26 18:20:32 +0100

    Opdateringer

--- .eleventy.js
+++ .eleventy.js
@@ -186,7 +186,7 @@ module.exports = function (eleventyConfig) {
     breaks: true,
     linkify: true,
   }).use(markdownItAnchor, {
-    permalink: true,
+    permalink: false,
     permalinkClass: "direct-link",
     permalinkSymbol: "#",
   });
